d8995cfa2571416e326300d4289e3a3bbab65dea
WildZarek <wildzarek@gmail.com>
2024-08-13 13:24:31 +0200

    First version of my 42env.sh file

--- 42env.sh
+++ 42env.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-COLOR_TURQUESA="\e[36m"
-COLOR_ROJO="\e[91m"
-COLOR_VERDE="\e[92m"
-COLOR_BLANCO="\e[97m"
+COLOR_YELLOW="\e[33m"
+COLOR_MAGENTA="\e[35m"
+COLOR_CYAN="\e[36m"
+COLOR_RED="\e[91m"
+COLOR_GREEN="\e[92m"
+COLOR_BLUE="\e[94m"
+COLOR_WHITE="\e[97m"
 COLOR_RESET="\e[0m"
 
 banner() {
-    echo -e "${COLOR_TURQUESA}"
+    echo -e "${COLOR_MAGENTA}"
     cat << "EOF"
     
   ┌─────────────────────────────────────────────────────────────┐
@@ -19,11 +22,12 @@ banner() {
   │            ╚═╝╚══════╝    ╚══════╝╚═╝  ╚═══╝  ╚═══╝         │
   └─────────────────────────────────────────────────────────────┘
 EOF
-    echo -e "${COLOR_RESET}"
-echo -e "${COLOR_BLANCO}  [+] 42ENV | 42 environment configuration script."
+echo -e "${COLOR_RESET}${COLOR_WHITE}"
+echo -e ">> 42ENV | 42 environment configuration script."
 echo ""
-echo -e "${COLOR_BLANCO}  [+] Andrés Lorente - andloren in 42Málaga."
-echo -e "${COLOR_BLANCO}  [+] https://github.com/4ndymcfly${COLOR_RESET}"
+echo -e ">> Si te sirvió este script, dale '★ Star' en el repositorio. ¡Gracias!"
+echo -e ">> ${COLOR_BLUE}https://github.com/WildZarek/42env${COLOR_RESET}"
+echo -e ">> ${COLOR_WHITE}forked from: ${COLOR_BLUE}https://github.com/4ndymcfly/42env${COLOR_RESET}"
 echo ""
 sleep 3
 }
@@ -32,38 +36,37 @@ clear
 banner
 
 print_info() {
-    echo -ne "${COLOR_TURQUESA}[+] $1 ${COLOR_RESET}"
+    echo -ne "${COLOR_WHITE}[${COLOR_CYAN}>${COLOR_WHITE}] $1${COLOR_RESET}"
 }
 
 print_installed() {
-    echo -e "${COLOR_TURQUESA}[i] $1${COLOR_RESET}"
+    echo -e "${COLOR_WHITE}[${COLOR_CYAN}i${COLOR_WHITE}] $1${COLOR_RESET}"
 }
 
 print_warning() {
-    echo -e "${COLOR_ROJO}[!] $1${COLOR_RESET}"
+    echo -e "${COLOR_WHITE}[${COLOR_RED}!${COLOR_WHITE}] $1${COLOR_RESET}"
 }
 
 print_ok() {
-    echo -e "${COLOR_VERDE}OK${COLOR_RESET}"
+    echo -e "${COLOR_GREEN}OK${COLOR_RESET}"
 }
-
 
 sudo -v
 while true; do sudo -n true; sleep 60; sudo -v; done 2>/dev/null &
 
-echo -ne "${COLOR_TURQUESA}[+] Escribe tu usuario de la intra de 42: ${COLOR_RESET}"
+print_info "Escribe tu usuario de la Intra 42:"
 read USER
 MAIL="${USER}@student.42malaga.com"
 
 check_and_install() {
     if ! command -v $1 &> /dev/null
     then
-        print_info "$1 no está instalado. Instalando..."
+        print_info "${COLOR_YELLOW}$1${COLOR_WHITE} no está instalado. Instalando..."
         sudo apt-get install -y $2 > /dev/null 2>&1
         sleep 2
         print_ok
     else
-        print_installed "$1 ya está instalado."
+        print_installed "${COLOR_YELLOW}$1${COLOR_WHITE} ya está instalado."
     fi
 }
 
@@ -74,14 +77,16 @@ sudo apt-get upgrade -y > /dev/null 2>&1
 sudo apt-get install -y software-properties-common build-essential curl wget unzip > /dev/null 2>&1
 print_ok
 
+check_and_install "zsh" "zsh"
+check_and_install "terminator" "terminator"
 check_and_install "git" "git"
 check_and_install "python3" "python3"
 check_and_install "pip3" "python3-pip"
 check_and_install "gcc" "gcc"
 check_and_install "make" "make"
-check_and_install "zsh" "zsh"
 check_and_install "ripgrep" "ripgrep"
 check_and_install "fd" "fd-find"
+check_and_install "nvim" "neovim"
 check_and_install "luarocks" "luarocks"
 
 #Instalaciones opcionales
@@ -93,50 +98,8 @@ check_and_install "luarocks" "luarocks"
 
 sleep 3
 
-if ! pip3 show norminette &> /dev/null
-then
-    print_info "norminette no está instalada. Instalando..."
-    pip3 install norminette > /dev/null 2>&1
-    sleep 2
-    print_ok
-else
-    print_installed "norminette ya está instalada."
-fi
-
-if ! pip3 show c-formatter-42 &> /dev/null
-then
-    print_info "c-formatter-42 no está instalado. Instalando..."
-    pip3 install c-formatter-42 > /dev/null 2>&1
-    sleep 2
-    print_ok
-else
-    print_installed "c-formatter-42 ya está instalado."
-fi
-
-if ! pip3 show neovim &> /dev/null
-then
-    print_info "Librería neovim para python no está instalada. Instalando..."
-    pip3 install neovim > /dev/null 2>&1
-    sleep 2
-    print_ok
-else
-    print_installed "Librería neovim ya está instalada."
-fi
-
-FONT_DIR="/usr/local/share/fonts"
-FONT_NAME="Hack"
-if ! fc-list | grep -q "$FONT_NAME"; then
-    print_info "Instalando Hack Nerd Font..."
-    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip -O /tmp/Hack.zip > /dev/null 2>&1
-    sudo unzip -q /tmp/Hack.zip -d $FONT_DIR > /dev/null 2>&1
-    sudo fc-cache -f -v > /dev/null 2>&1
-    print_ok
-else
-    print_installed "Hack Nerd Font ya está instalada."
-fi
-
 if [ "$SHELL" != "$(which zsh)" ]; then
-    print_info "Configurando zsh como shell por defecto..."
+    print_info "Configurando ${COLOR_YELLOW}zsh${COLOR_WHITE} como shell por defecto..."
     sudo chsh -s $(which zsh) $(whoami)
     sleep 2
     print_ok
@@ -146,110 +109,120 @@ ZSHRC_FILE="$HOME/.zshrc"
 if [ ! -f "$ZSHRC_FILE" ]; then
     # Copiar el archivo .zshrc desde la raíz del script si no existe
     if [ -f "./files/.zshrc" ]; then
-        print_info "Creando archivo .zshrc desde el archivo de configuración en la raíz del script..."
+        print_info "Copiando archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE} desde el archivo de configuración en la raíz del script..."
         cp "./files/.zshrc" "$ZSHRC_FILE"
         sleep 2
         print_ok
     else
-        print_warning "No se encontró el archivo .zshrc en la ruta actual."
+        print_warning "No se encontró el archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE} en la ruta actual."
     fi
 else
     # Añadir los alias al .zshrc si el archivo ya existe
-    print_info "Añadiendo alias a .zshrc..."
-    echo -e "\n# Alias\nalias 42gcc='gcc -Wall -Werror -Wextra'\nalias 42nt='norminette -R CheckForbiddenSourceHeader'\nalias ll='/usr/bin/lsd -lha --group-dirs=first'\nalias llo='/usr/bin/lsd -lha --group-dirs=first --permission octal'\nalias ls='/usr/bin/lsd --group-dirs=first'\nalias vi='/snap/bin/nvim'\nalias picture='kitty +kitten icat'" >> "$ZSHRC_FILE"
+    print_info "Añadiendo los alias al archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE}..."
+    echo -e "\n# ALIASES\nalias caa='cat */*'\nalias cca='cc -Wall -Wextra -Werror -o $1 *.c'\nalias ccf='cc -Wall -Wextra -Werror $1'\nalias ccg='cc -Wall -Wextra -Werror -g -o $1'\nalias gcf='gcc -Wall -Wextra -Werror -o $1'\nalias gts='git status'\nalias gta='git add $1'\nalias gtaa='git add .'\nalias gtc='git commit -m $1'\nalias gtpu='git push -u origin master'\nalias gtp='git push'\nalias gtd='git diff $1'\nalias gtl=\"git log --format='%h | %ce | %cs | %ch' -n10\"\nalias ll='/usr/bin/lsd -lha --group-dirs=first'\nalias llo='/usr/bin/lsd -lha --group-dirs=first --permission octal'\nalias ls='/usr/bin/lsd --group-dirs=first'\nalias nt='norminette -R CheckForbiddenSourceHeader'\nalias fl='ft_lock'\nalias vi='/usr/bin/nvim'\n" >> "$ZSHRC_FILE"
     sleep 2
     print_ok
 fi
 
-KITTY_CONF_DIR="$HOME/.config/kitty"
-KITTY_CONF_SRC="./files/kitty.conf"
-KITTY_CONF_DEST="$KITTY_CONF_DIR/kitty.conf"
+#Instalación de Oh-my-Zsh y plugins estilo fish
+print_info "Instalando ${COLOR_YELLOW}Oh-My-Zsh${COLOR_WHITE}..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 2
+print_ok
 
-mkdir -p "$KITTY_CONF_DIR"
-if [ -f "$KITTY_CONF_SRC" ]; then
-    cp "$KITTY_CONF_SRC" "$KITTY_CONF_DEST"
+print_info "Instalando plugin ${COLOR_YELLOW}zsh-autosuggestions${COLOR_WHITE}..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sleep 2
+print_ok
+
+print_info "Instalando plugin ${COLOR_YELLOW}zsh-syntax-highlighting${COLOR_WHITE}..."
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sleep 2
+print_ok
+
+if ! pip3 show norminette &> /dev/null
+then
+    print_info "${COLOR_YELLOW}norminette${COLOR_WHITE} no está instalada. Instalando..."
+    pip3 install norminette > /dev/null 2>&1
     sleep 2
+    print_ok
 else
-    print_warning "No se encontró el archivo kitty.conf en la ruta actual."
+    print_installed "${COLOR_YELLOW}norminette${COLOR_WHITE} ya está instalada."
 fi
 
-print_info "Instalando Kitty..."
-curl -sL https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin > /dev/null 2>&1
-
-mkdir -p ~/.local/bin
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/bin/kitty
-ln -sf ~/.local/kitty.app/bin/kitten ~/.local/bin/kitten
-
-export PATH="$HOME/.local/bin:$PATH"
-
-mkdir -p ~/.local/share/applications
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-
-sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
-sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty.desktop
-
-echo 'kitty.desktop' > ~/.config/xdg-terminals.list
-sleep 2
-killall kitty
-print_ok
-
-print_info "Instalando Neovim..."
-sudo snap install --classic nvim > /dev/null 2>&1
-sleep 2
-print_ok
-
-print_info "Instalando y configurando LazyVim + plugins..."
-# Copia si existen de los archivos actuales de Neovim
-mv ~/.config/nvim{,.bak} > /dev/null 2>&1
-mv ~/.local/share/nvim{,.bak} > /dev/null 2>&1
-mv ~/.local/state/nvim{,.bak} > /dev/null 2>&1
-mv ~/.cache/nvim{,.bak} > /dev/null 2>&1
-
-git clone https://github.com/LazyVim/starter ~/.config/nvim > /dev/null 2>&1
-rm -rf ~/.config/nvim/.git > /dev/null 2>&1
-sleep 3
-
-LAZY_LUA_SRC="./files/lazy.lua"
-LAZY_LUA_DEST="$HOME/.config/nvim/lua/config/lazy.lua"
-
-if [ -f "$LAZY_LUA_SRC" ]; then
-    cp "$LAZY_LUA_SRC" "$LAZY_LUA_DEST"
-    sed -i "s/USERHEADER/$USER/g" "$LAZY_LUA_DEST"
-    sed -i "s/MAILHEADER/$MAIL/g" "$LAZY_LUA_DEST"
+if ! pip3 show c-formatter-42 &> /dev/null
+then
+    print_info "${COLOR_YELLOW}c-formatter-42${COLOR_WHITE} no está instalado. Instalando..."
+    pip3 install c-formatter-42 > /dev/null 2>&1
+    sleep 2
+    print_ok
 else
-    print_warning "No se encontró el archivo lazy.lua en la ruta actual."
+    print_installed "${COLOR_YELLOW}c-formatter-42${COLOR_WHITE} ya está instalado."
+fi
+
+if ! pip3 show neovim &> /dev/null
+then
+    print_info "Librería ${COLOR_WHITE}neovim${COLOR_WHITE} para Python no está instalada. Instalando..."
+    pip3 install neovim > /dev/null 2>&1
+    sleep 2
+    print_ok
+else
+    print_installed "Librería ${COLOR_YELLOW}neovim${COLOR_WHITE} ya está instalada."
+fi
+
+FONT_DIR="/usr/local/share/fonts"
+FONT_NAME="Hack"
+if ! fc-list | grep -q "$FONT_NAME"; then
+    print_info "Instalando ${COLOR_YELLOW}Hack Nerd Font${COLOR_WHITE}..."
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hack.zip -O /tmp/Hack.zip > /dev/null 2>&1
+    sudo unzip -q /tmp/Hack.zip -d $FONT_DIR > /dev/null 2>&1
+    sudo fc-cache -f -v > /dev/null 2>&1
+    print_ok
+else
+    print_installed "${COLOR_YELLOW}Hack Nerd Font${COLOR_WHITE} ya está instalada."
 fi
 
 KEYMAPS_LUA_SRC="./files/keymaps.lua"
-KEYMAPS_LUA_DEST="$HOME/.config/nvim/lua/config/keymaps.lua"
+KEYMAPS_LUA_DEST="$HOME/.config/nvim/lua/config/"
 
 if [ -f "$KEYMAPS_LUA_SRC" ]; then
-    cp "$KEYMAPS_LUA_SRC" "$KEYMAPS_LUA_DEST"
-    sleep 1
+    print_info "Copiando archivo ${COLOR_YELLOW}keymaps.lua${COLOR_WHITE} al directorio de configuración de nvim..."
+    if [ -d $KEYMAPS_LUA_DEST ]; then
+        cp "$KEYMAPS_LUA_SRC" "$KEYMAPS_LUA_DEST"
+        sleep 1
+        print_ok
+    else:
+        print_error "No se encontró el directorio de configuración de nvim. Creando los directorios..."
+        mkdir -p $KEYMAPS_LUA_DEST
+        cp "$KEYMAPS_LUA_SRC" "$KEYMAPS_LUA_DEST"
+        sleep 1
+        print_ok
+    fi
 else
-    print_warning "No se encontró el archivo keymaps.lua en la ruta actual."
+        print_warning "No se encontró el archivo ${COLOR_YELLOW}keymaps.lua${COLOR_WHITE} en la ruta actual."
 fi
 
 FORMATTER_42_SRC="./files/c_formatter_42.vim"
-FORMATTER_42_DEST="$HOME/.config/nvim/lua/plugins/c_formatter_42.vim"
+FORMATTER_42_DEST="$HOME/.config/nvim/lua/plugins/"
 
 if [ -f "$FORMATTER_42_SRC" ]; then
-    cp "$FORMATTER_42_SRC" "$FORMATTER_42_DEST"
-    sleep 1
+    print_info "Copiando plugin ${COLOR_YELLOW}c_formatter_42.vim${COLOR_WHITE} al directorio de plugins de nvim..."
+    if [ -d $FORMATTER_42_DEST ]; then
+        cp "$FORMATTER_42_SRC" "$FORMATTER_42_DEST"
+        sleep 1
+        print_ok
+    else
+        print_error "No se encontró el directorio de plugins de nvim. Creando los directorios..."
+        mkdir -p $FORMATTER_42_DEST
+        cp "$FORMATTER_42_SRC" "$FORMATTER_42_DEST"
+        sleep 1
+        print_ok
+    fi
 else
-    print_warning "No se encontró el archivo c_formatter_42.vim en la ruta actual."
+    print_warning "No se encontró el archivo ${COLOR_YELLOW}c_formatter_42.vim${COLOR_WHITE} en la ruta actual."
 fi
-print_ok
 
-print_info "Añadiendo Kitty a la barra de favoritos..."
-CURRENT_FAVORITES=$(gsettings get org.gnome.shell favorite-apps)
-NEW_FAVORITES=$(echo $CURRENT_FAVORITES | sed "s/]/, 'kitty.desktop']/")
-gsettings set org.gnome.shell favorite-apps "$NEW_FAVORITES"
-sleep 1
-print_ok
-
-print_info "Instalando LazyGit..."
+print_info "Instalando ${COLOR_YELLOW}LazyGit${COLOR_WHITE}..."
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" > /dev/null 2>&1
@@ -259,7 +232,7 @@ rm lazygit.tar.gz > /dev/null 2>&1
 sleep 1
 print_ok
 
-print_info "Instalando LSD..."
+print_info "Instalando ${COLOR_YELLOW}LSD${COLOR_WHITE}..."
 LSD_URL="https://github.com/lsd-rs/lsd/releases/download/v1.1.2/lsd-musl_1.1.2_amd64.deb"
 LSD_DEB="lsd-musl_1.1.2_amd64.deb"
 
@@ -278,11 +251,11 @@ echo ""
 echo ""
 
 sleep 1
-print_installed "[ATENCION] El sistema se reiniciará para aplicar todos los cambios. Después del reinicio, abre Kitty y desde ahí ejecuta 'nvim' para que la configuración de Neovim se complete."
+print_installed "[ATENCIÓN] El sistema se reiniciará para aplicar todos los cambios. Después del reinicio, ejecuta 'nvim' para que la configuración de Neovim se complete."
 
 echo ""
 echo ""
 
 sleep 1
-read -p "Pulse intro para reiniciar el sistema..."
+read -p "Pulse la tecla INTRO para reiniciar el sistema..."
 sudo reboot

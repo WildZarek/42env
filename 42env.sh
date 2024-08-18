183917357ec9250da461d66637898bebac6f3ca6
WildZarek <wildzarek@gmail.com>
2024-08-18 02:26:28 +0200

    Added a lot of checks and fixes

--- 42env.sh
+++ 42env.sh
@@ -56,6 +56,10 @@ print_pass() {
     echo -e "${COLOR_BLUE}PASS${COLOR_RESET}"
 }
 
+print_error() {
+    echo -e "${COLOR_RED}ERROR${COLOR_RESET}"
+}
+
 sudo -v
 while true; do sudo -n true; sleep 60; sudo -v; done 2>/dev/null &
 
@@ -103,11 +107,10 @@ check_and_install "luarocks" "luarocks"
 
 sleep 3
 
-# Fix for 'fdfind' command in Ubuntu and Debian
+# Fix for 'fdfind' command in Debian
 FD_SYMLINK_PATH="$HOME/.local/bin/fd"
-export PATH="$HOME/.local/bin:$PATH"
 if [ ! $(which fd) ]; then
-    if [ ! -d FD_SYMLINK_PATH ]; then
+    if [ ! -L $FD_SYMLINK_PATH ]; then
         ln -s $(which fdfind) $FD_SYMLINK_PATH
     fi
 fi
@@ -119,44 +122,26 @@ if [ "$SHELL" != "$(which zsh)" ]; then
     print_ok
 fi
 
-ZSHRC_SRC="./files/.zshrc"
-ZSHRC_DEST="$HOME/.zshrc"
-ALIASES_FILE="./files/aliases.txt"
-if [ ! -f "$ZSHRC_DEST" ]; then
-    if [ -f $ZSHRC_SRC ]; then
-        print_info "Copiando el archivo de configuración ${COLOR_YELLOW}.zshrc${COLOR_WHITE}..."
-        cp "$ZSHRC_SRC" "$ZSHRC_DEST"
-        sleep 2
-        print_ok
-    else
-        print_warning "No se encontró el archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE} en la ruta de origen.\n"
-    fi
-else
-    # Añadir los alias al .zshrc si el archivo ya existe y no existen otros alias definidos
-    if ! grep -q "ALIASES" "$ZSHRC_DEST"; then
-        print_info "Añadiendo los alias al archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE}..."
-        cat "$ALIASES_FILE" >> "$ZSHRC_FILE"
-        sleep 2
-        print_ok
-    else
-        print_warning "Ya existen alias definidos. Cancelando configuración..."
-        sleep 1
-        print_pass
-    fi
-fi
-
-#Instalación de Oh-my-Zsh y plugins estilo fish
-if [ ! -d $ZSH ]; then
+#Instalación de Oh-My-Zsh y plugins estilo fish
+OMZ_SH_FILE="$HOME/.oh-my-zsh/oh-my-zsh.sh"
+if [ ! -f $OMZ_SH_FILE ]; then
     print_info "Instalando ${COLOR_YELLOW}Oh-My-Zsh${COLOR_WHITE}..."
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
     sleep 2
-    print_ok
+    # Doble validación por si no se instaló correctamente...
+    if [ -f $OMZ_SH_FILE ]; then
+        print_ok
+    else
+        print_warning "${COLOR_YELLOW}Oh-My-Zsh${COLOR_WHITE} no se instaló correctamente..."
+        sleep 1
+        print_pass
+    fi
 else
     print_installed "Paquete ${COLOR_YELLOW}Oh-My-Zsh${COLOR_WHITE} ya está instalado."
 fi
 
 ZSH_PLUGIN1_PATH="$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
-if [ ! -d $ZSH_PLUGIN1_PATH ]; then
+if [ -d $ZSH ] && [ ! -d $ZSH_PLUGIN1_PATH ]; then
     print_info "Instalando plugin ${COLOR_YELLOW}zsh-autosuggestions${COLOR_WHITE}..."
     git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PLUGIN1_PATH > /dev/null 2>&1
     sleep 2
@@ -166,13 +151,41 @@ else
 fi
 
 ZSH_PLUGIN2_PATH="$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
-if [ ! -d $ZSH_PLUGIN2_PATH ]; then
+if [ -d $ZSH ] && [ ! -d $ZSH_PLUGIN2_PATH ]; then
     print_info "Instalando plugin ${COLOR_YELLOW}zsh-syntax-highlighting${COLOR_WHITE}..."
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_PLUGIN2_PATH > /dev/null 2>&1
     sleep 2
     print_ok
 else
     print_installed "Plugin ${COLOR_YELLOW}zsh-syntax-highlighting${COLOR_WHITE} ya está instalado."
+fi
+
+ZSHRC_SRC="./files/.zshrc"
+ZSHRC_DEST="$HOME/.zshrc"
+ALIASES_FILE="./files/aliases.txt"
+if [ -f "$ZSHRC_SRC" ]; then
+    if [ -f $ZSHRC_DEST ]; then
+        print_info "Copiando el archivo de configuración ${COLOR_YELLOW}.zshrc${COLOR_WHITE}..."
+        mv "$ZSHRC_DEST" ".zshrc.pre-42env"
+        cp "$ZSHRC_SRC" "$ZSHRC_DEST"
+        sleep 2
+        print_ok
+        # Añadir los alias al .zshrc si el archivo ya existe y no existen otros alias definidos
+        if ! grep -q "ALIASES" "$ZSHRC_DEST"; then
+            print_info "Añadiendo los alias al archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE}..."
+            cat "$ALIASES_FILE" >> "$ZSHRC_DEST"
+            sleep 2
+            print_ok
+        else
+            print_warning "Ya existen alias definidos. Cancelando configuración..."
+            sleep 1
+            print_pass
+        fi
+    fi
+else
+    print_warning "No se encontró el archivo ${COLOR_YELLOW}.zshrc${COLOR_WHITE} en la ruta de origen..."
+    sleep 1
+    print_error
 fi
 
 if ! pip3 show norminette &> /dev/null
@@ -219,17 +232,25 @@ fi
 
 # Configuración de Neovim + Plugins
 # Plugins Path: $HOME/.local/share/nvim/plugged
-if [ command -v nvim ]; then
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+VIMPLUG_FILE="$HOME/.local/share/nvim/site/autoload/plug.vim"
+if [ $(which nvim) ]; then
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' > /dev/null 2>&1
+    if [ -f $VIMPLUG_FILE ]; then
+        print_info "Gestor de plugins ${COLOR_YELLOW}Vim-Plug${COLOR_WHITE} para NeoVim instalado..."
+        sleep 1
+        print_ok
+    else
+        print_warning "El gestor de plugins ${COLOR_YELLOW}Vim-Plug${COLOR_WHITE} para NeoVim no se instaló correctamente..."
+        sleep 1
+        print_error
 else
-    print_warning "NeoVim no se instaló correctamente..."
+    print_warning "${COLOR_YELLOW}NeoVim${COLOR_WHITE} no se instaló correctamente..."
     sleep 1
-    print_pass
+    print_error
 fi
 
 NVIM_SRC="./files/init.vim"
-NVIM_DEST="$HOME/.config/nvim/lua/"
+NVIM_DEST="$HOME/.config/nvim/lua"
 NVIM_CFG_FILE="$NVIM_DEST/init.vim"
 if [ -f $NVIM_SRC ]; then
     if [ ! -d $NVIM_DEST ]; then
@@ -245,10 +266,10 @@ if [ -f $NVIM_SRC ]; then
             print_installed "Archivo ${COLOR_YELLOW}init.vim${COLOR_WHITE} ya existente. Cancelando copia..."
             sleep 1
             print_pass
-            if [ ! $(grep -q "let g:user42" $NVIM_DEST) ] && [ ! $(grep -q "let g:mail42" $NVIM_DEST) ]; then
+            if [ ! $(grep -q "let g:user42" $NVIM_CFG_FILE) ] && [ ! $(grep -q "let g:mail42" $NVIM_CFG_FILE) ]; then
                 print_info "Configurando variables para el header de 42..."
-                echo -e "let g:user42 = '${INTRAUSER}'" >> $NVIM_DEST
-                echo -e "let g:mail42 = '${INTRAUSER}@student.42malaga.com'" >> $NVIM_DEST
+                echo -e "let g:user42 = '${INTRAUSER}'" >> $NVIM_CFG_FILE
+                echo -e "let g:mail42 = '${INTRAUSER}@student.42malaga.com'" >> $NVIM_CFG_FILE
                 sleep 1
                 print_ok
             else
@@ -257,7 +278,9 @@ if [ -f $NVIM_SRC ]; then
         fi
     fi
 else
-    print_warning "No se encontró el archivo ${COLOR_YELLOW}init.vim${COLOR_WHITE} en la ruta de origen.\n"
+    print_warning "No se encontró el archivo ${COLOR_YELLOW}init.vim${COLOR_WHITE} en la ruta de origen..."
+    sleep 1
+    print_error
 fi
 
 KEYMAPS_LUA_SRC="./files/keymaps.lua"
@@ -279,29 +302,9 @@ if [ -f "$KEYMAPS_LUA_SRC" ]; then
         print_ok
     fi
 else
-    print_warning "No se encontró el archivo ${COLOR_YELLOW}keymaps.lua${COLOR_WHITE} en la ruta actual.\n"
-fi
-
-FORMATTER_42_SRC="./files/c_formatter_42.vim"
-FORMATTER_42_DEST="$HOME/.config/nvim/lua/plugins/"
-if [ -f "$FORMATTER_42_SRC" ]; then
-    if [ -d $FORMATTER_42_DEST ]; then
-        print_info "Copiando plugin ${COLOR_YELLOW}c_formatter_42.vim${COLOR_WHITE} al directorio de plugins de nvim..."
-        cp "$FORMATTER_42_SRC" "$FORMATTER_42_DEST"
-        sleep 1
-        print_ok
-    else
-        print_warning "No se encontró el directorio 'plugins' de nvim. Creando los directorios..."
-        mkdir -p $FORMATTER_42_DEST
-        sleep 1
-        print_ok
-        print_info "Copiando plugin ${COLOR_YELLOW}c_formatter_42.vim${COLOR_WHITE} al directorio de plugins de nvim..."
-        cp "$FORMATTER_42_SRC" "$FORMATTER_42_DEST"
-        sleep 1
-        print_ok
-    fi
-else
-    print_warning "No se encontró el archivo ${COLOR_YELLOW}c_formatter_42.vim${COLOR_WHITE} en la ruta actual.\n"
+    print_warning "No se encontró el archivo ${COLOR_YELLOW}keymaps.lua${COLOR_WHITE} en la ruta actual..."
+    sleep 1
+    print_error
 fi
 
 if [ ! $(command -v lazygit) ]; then
@@ -311,6 +314,7 @@ if [ ! $(command -v lazygit) ]; then
     tar xf lazygit.tar.gz lazygit > /dev/null 2>&1
     sudo install lazygit /usr/local/bin > /dev/null 2>&1
     rm lazygit.tar.gz > /dev/null 2>&1
+    rm lazygit > /dev/null 2>&1
     sleep 1
     print_ok
 else
